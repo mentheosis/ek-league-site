0735363d77d19868ea9d6648f5bfe4b7111ac722
Kris <mentheosis@gmail.com>
2015-02-19 19:36:06 -0500

    build new countdown

--- public/dist/application.js
+++ public/dist/application.js
@@ -544,11 +544,49 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$sce', '$rootScope', 'Authentication', 'Settings', 'Menus',
-	function($scope, $sce, $rootScope, Authentication, Settings, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', '$sce', '$rootScope', '$timeout', 'Authentication', 'Settings', 'Menus',
+	function($scope, $sce, $rootScope, $timeout, Authentication, Settings, Menus) {
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
+
+
+
+    $scope.targetDate = new Date(2015, 2, 7, 12);
+    $scope.now = new Date();
+		$scope.showCountdownLogin = false;
+
+    function dateDiff(now, later) {
+      var diffMs = later - now;
+
+      var secMs = 1000;
+      var minMs = secMs * 60;
+      var hourMs = minMs * 60;
+      var dayMs = hourMs * 24;
+
+      var diffDays = Math.floor(diffMs / dayMs);
+      var diffHours = Math.floor((diffMs % dayMs) / hourMs);
+      var diffMins = Math.floor((diffMs % hourMs) / minMs);
+      var diffSec = Math.floor((diffMs % minMs) / secMs);
+
+      return {
+        day: diffDays.toString(),
+        hour: diffHours.toString(),
+        min: diffMins.toString(),
+        sec: diffSec.toString()
+        }
+    }
+
+    $scope.counter = dateDiff($scope.now, $scope.targetDate);
+
+    $scope.onTimeout = function(){
+        $scope.now = new Date();
+        $scope.counter = dateDiff($scope.now, $scope.targetDate);
+        mytimeout = $timeout($scope.onTimeout,1000);
+    }
+    var mytimeout = $timeout($scope.onTimeout,1000);
+
+
 
 		$rootScope.adminMode = false;
 		$scope.switchAdminMode = function(){
